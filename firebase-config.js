62a0e68bbe7e6b979ef73598418b5050c8176416
cweed <weed.christopher@gmail.com>
2026-06-11 09:41:15 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.BEASTAVE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC7hM9XPm-5DKdNamLGCFKQWXu0IjJwbaw",
+  authDomain: "scoreboard-329ca.firebaseapp.com",
+  databaseURL: "https://scoreboard-329ca-default-rtdb.firebaseio.com/",
+  projectId: "scoreboard-329ca",
+  storageBucket: "scoreboard-329ca.firebasestorage.app",
+  messagingSenderId: "149956786283",
+  appId: "1:149956786283:web:5efe66181d0b64fa69592e"
+};
+
+window.BEASTAVE_DEFAULT_GAME_ID = "week-1";
